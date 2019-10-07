24a502c1c0a29eab308f1650f04e716d171cc546
asojka09 <austinsojka@gmail.com>
2019-10-07 08:54:23 -0500

    Added the core PlayFab() structure

--- targets/brightscript/make.js
+++ targets/brightscript/make.js
@@ -4,35 +4,26 @@ var path = require("path");
 if (typeof (getCompiledTemplate) === "undefined") getCompiledTemplate = function () { };
 if (typeof (templatizeTree) === "undefined") templatizeTree = function () { };
 
-// generate.js looks for some specific exported functions (as defined in TOC.json) in make.js, like:
-exports.makeClientAPI2 = function (apis, sourceDir, apiOutputDir) {
-    // Builds the client api.  The provided "api" variable is a single object, the API_SPECS/client.api.json as an object
+exports.makeCombinedAPI = function (apis, sourceDir, apiOutputDir) {
+    // Builds every api.  The provided "apis" variable is a list of objects, Examples: API_SPECS/Legacy/PlayFab/admin.api.json, API_SPECS/Legacy/PlayFab/server.api.json, and API_SPECS/Legacy/PlayFab/client.api.json
     
     console.log("Generating Client api from: " + sourceDir + " to: " + apiOutputDir);
     templatizeTree({}, path.resolve(sourceDir, "source"), apiOutputDir); // Copy the whole source directory as-is
+    makePlayFab(apis, sourceDir,apiOutputDir)
     for (var i = 0; i < apis.length; i++) {
         makeApi(apis[i], sourceDir, apiOutputDir);
     }
 }
 
-// // generate.js looks for some specific exported functions (as defined in TOC.json) in make.js, like:
-// exports.makeServerAPI = function (apis, sourceDir, apiOutputDir) {
-//     // Builds the server api.  The provided "apis" variable is a list of objects, Examples: API_SPECS/Legacy/PlayFab/admin.api.json and API_SPECS/Legacy/PlayFab/server.api.json
-//     // If you don't want admin, you should filter it out yourself (for now)
-    
-//     console.log("Generating Server api from: " + sourceDir + " to: " + apiOutputDir);
-//     templatizeTree({}, path.resolve(sourceDir, "source"), apiOutputDir); // Copy the whole source directory as-is
-//     MakeExampleTemplateFile(sourceDir, apiOutputDir);
-// }
+function makePlayFab(apis, sourceDir, apiOutputDir) {
+    var locals = {
+        apis: apis,
+        sdkVersion: sdkGlobals.sdkVersion
+    };
 
-// // generate.js looks for some specific exported functions (as defined in TOC.json) in make.js, like:
-// exports.makeCombinedAPI = function (apis, sourceDir, apiOutputDir) {
-//     // Builds every api.  The provided "apis" variable is a list of objects, Examples: API_SPECS/Legacy/PlayFab/admin.api.json, API_SPECS/Legacy/PlayFab/server.api.json, and API_SPECS/Legacy/PlayFab/client.api.json
-    
-//     console.log("Generating Combined api from: " + sourceDir + " to: " + apiOutputDir);
-//     templatizeTree({}, path.resolve(sourceDir, "source"), apiOutputDir); // Copy the whole source directory as-is
-//     MakeExampleTemplateFile(sourceDir, apiOutputDir);
-// }
+    var apiTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/PlayFab.brs.ejs"));
+    writeFile(path.resolve(apiOutputDir, "PlayFab.brs"), apiTemplate(locals));
+}
 
 function makeApi(api, sourceDir, apiOutputDir) {
     var locals = {
--- targets/brightscript/source/exampleSourceFile.brs
+++
@@ -1,4 +0,0 @@
-function PlayFabSomethingOrAnother()
-end function
-
-
